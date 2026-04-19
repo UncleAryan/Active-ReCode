2a418f753a045e73ded5a840d3c1cb9307c323df
Erik-Ecklund <168017305+Erik-Ecklund@users.noreply.github.com>
2026-04-19 19:00:08 -0400

    added codeRunner unit tests
    there is currently an issue with them all failing
    Web Workers do not test well

--- tests/setup.js
+++ tests/setup.js
@@ -1,2 +1,15 @@
 // just so we can add more matchers to expect behaviours
 import "@testing-library/jest-dom";
+import "fake-indexeddb/auto";
+import "@vitest/web-worker";
+import { beforeEach } from "vitest";
+
+// clear out indexedDB before each test
+beforeEach( async () => {
+    await new Promise((resolve, reject) => {
+    const req = indexedDB.deleteDatabase("CodeRunnerDB");
+    req.onsuccess = () => resolve();
+    req.onerror = () => reject(req.error);
+    req.onblocked = () => resolve();
+  });
+});
---
+++ tests/unit/codeRunner.test.js
@@ -0,0 +1,73 @@
+import { runUserCode } from "../../src/logic/codeRunner.js";
+import { describe, it, expect } from "vitest";
+import '@vitest/web-worker'
+
+describe("codeRunner unit tests", () => {
+  it("Happy Path", async () => {
+    const result = await runUserCode({
+      userCode: "function add(a, b) { return a + b; }",
+      functionName: "add",
+      testInput: "2, 3",
+      expectedOutput: 5,
+      timeoutMs: 2000,
+    });
+
+    expect(result.passed).toBe(true);
+    expect(result.actual).toBe(5);
+    expect(result.error).toBeNull();
+    expect(result.timeTaken).toMatch(/\d+\.\d+ ms/);
+  });
+
+  it("Rainy Day Path : incorrect output", async () => {
+    const result = await runUserCode({
+      userCode: "function add(a, b) { return a + b; }",
+      functionName: "add",
+      testInput: "2, 3",
+      expectedOutput: 999,
+      timeoutMs: 2000,
+    });
+
+    expect(result.passed).toBe(false);
+    expect(result.actual).toBe(5);
+  });
+
+  it("Rainy Day Path: runtime error", async () => {
+    const result = await runUserCode({
+      userCode: "function bad() { throw new Error('user made a mistake'); }",
+      functionName: "bad",
+      testInput: "",
+      expectedOutput: null,
+      timeoutMs: 2000,
+    });
+
+    expect(result.passed).toBe(false);
+    expect(result.error).toContain("user made a mistake");
+  });
+
+  it("Rainy Day Path : infinite loop", async () => {
+    const result = await runUserCode({
+      userCode: "function infinite() { while(true) {} }",
+      functionName: "infinite",
+      testInput: "",
+      expectedOutput: null,
+      timeoutMs: 100,
+    });
+
+    expect(result.passed).toBe(false);
+    expect(result.error).toContain("timed out");
+  });
+
+  it("Rainy Day Path : used wrong function name", async () => {
+    const result = await runUserCode({
+      userCode: "function add(a, b) { return a + b; }",
+      functionName: "subtract",
+      testInput: "2, 3",
+      expectedOutput: 5,
+      timeoutMs: 2000,
+    });
+
+    expect(result.passed).toBe(false);
+    expect(result.error).toContain("Cannot find");
+  });
+
+});
