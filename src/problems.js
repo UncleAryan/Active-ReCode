08ca92c7028c44e63375576841d5326570d0e8c3
Carson Howard <carhoward18@gmail.com>
2026-04-20 13:28:29 -0400

    Add problem: Number of 1 Bits (Easy)

--- src/problems.js
+++ src/problems.js
@@ -321,6 +321,43 @@ Constraints:
 
 }`,
   },
+  {
+    title: "Number of 1 Bits",
+    difficulty: "Easy",
+    dateAdded: "04/20/2026",
+    description: `Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+Example 1:
+  Input: n = 11
+  Output: 3
+  Explanation: The input binary string 1011 has a total of three set bits.
+
+Example 2:
+  Input: n = 128
+  Output: 1
+  Explanation: The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+  Input: n = 2147483645
+  Output: 30
+  Explanation: The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+
+Constraints:
+  • 1 <= n <= 2^31 - 1
+
+Follow up:
+  If this function is called many times, how would you optimize it?`,
+    functionName: "hammingWeight",
+    testCases: [
+      { input: "11", expected: 3 },
+      { input: "128", expected: 1 },
+      { input: "2147483645", expected: 30 },
+    ],
+    starterCode: `function hammingWeight(n) {
+  // Your solution here
+
+}`,
+  },
 ];
 
 export default problems;
